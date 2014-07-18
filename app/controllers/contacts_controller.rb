47e55c03e27f3e157e0c6f40def898cb8545001a
Marcel Sands <marcel.sands84@gmail.com>
2014-07-18 10:49:46 -0400

    generate usermailer and modify contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,19 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      UserMailer.contact_message(@contact).deliver
+      redirect_to root_path, notice: 'Your message has been sent !'
+    else
+      render action: 'new'
   end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :subject, :message)
+    end
 end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,16 @@
+class UserMailer < ActionMailer::Base
+  default from: "noreply@JackieConsignsDecatur.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.contact_message.subject
+  #
+  def contact_message(contact)
+    @contact = contact
+
+    mail to: "suberina.jocelyn@gmail.com",
+      cc: contact.email,
+      subject: contact.subject
+  end
+end
