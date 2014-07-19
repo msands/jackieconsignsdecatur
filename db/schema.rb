9922b190ceb1c23c773a23041770e453aaa45086
Marcel Sands <marcel.sands84@gmail.com>
2014-07-19 02:49:27 -0400

    rake migration and modify product and user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140719061518) do
+ActiveRecord::Schema.define(version: 20140719064412) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(version: 20140719061518) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "products", ["user_id"], name: "index_products_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
