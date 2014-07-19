2d74ccbded7311b53975c17f6b020ed2b27ce805
Marcel Sands <marcel.sands84@gmail.com>
2014-07-19 04:00:56 -0400

    add image upload to product form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140719064412) do
+ActiveRecord::Schema.define(version: 20140719075717) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(version: 20140719064412) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "products", ["user_id"], name: "index_products_on_user_id"
