05bf3d769f7dade513f5cdc77f8f630057c07de3
Marcel Sands <marcel.sands84@gmail.com>
2014-07-20 12:40:23 -0400

    add image upload with paperclip

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -53,6 +53,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:description)
+      params.require(:product).permit(:description, :image)
     end
 end
